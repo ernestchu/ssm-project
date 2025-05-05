6dfb6082b4df5732a7228e437d55b66e8baef714
Ernie Chu <schu23@jhu.edu>
2025-05-05 09:53:50 -0400

    add results

--- task1/calc_accuracy.py
+++ task1/calc_accuracy.py
@@ -8,16 +8,19 @@ models = [
     ["meta-llama/Llama-3.2-3B-Instruct", "Llama3.2-3B-it"],
     ["google/gemma-2-2b", "Gemma2-2B"],
     ["google/gemma-2-2b-it", "Gemma2-2B-it"],
+    r'\midrule',
     ["Qwen/Qwen2.5-7B", "Qwen2.5-7B"],
     ["Qwen/Qwen2.5-7B-Instruct", "Qwen2.5-7B-it"],
-    # ["meta-llama/Llama-3.1-8B", "Llama3.1-8B"],
-    # ["meta-llama/Llama-3.1-8B-Instruct", "Llama3.1-8B-it"],
-    # ["google/gemma-2-9b", "Gemma2-9B"],
-    # ["google/gemma-2-9b-it", "Gemma2-9B-it"],
-    # ["Qwen/Qwen3-4B-Base", "Qwen3-4B"],
-    # ["Qwen/Qwen3-4B", "Qwen3-4B-it"],
-    # ["google/gemma-3-4b-pt", "Gemma3-4B"],
-    # ["google/gemma-3-4b-it", "Gemma3-4B-it"],
+    ["meta-llama/Llama-3.1-8B", "Llama3.1-8B"],
+    ["meta-llama/Llama-3.1-8B-Instruct", "Llama3.1-8B-it"],
+    ["google/gemma-2-9b", "Gemma2-9B"],
+    ["google/gemma-2-9b-it", "Gemma2-9B-it"],
+    r'\midrule',
+    ["Qwen/Qwen3-4B-Base", "Qwen3-4B"],
+    ["Qwen/Qwen3-4B", "Qwen3-4B-it"],
+    ["google/gemma-3-4b-pt", "Gemma3-4B"],
+    ["google/gemma-3-4b-it", "Gemma3-4B-it"],
+    r'\midrule',
     # ["Qwen/Qwen3-14B-Base", "Qwen3-14B"],
     # ["Qwen/Qwen3-14B", "Qwen3-14B-it"],
     # ["google/gemma-3-12b-pt", "Gemma3-12B"],
@@ -25,7 +28,11 @@ models = [
 ]
 
 table = ""
-for model_name, model_name_canonical in models:
+for tup in models:
+    if isinstance(tup, str):
+        table += f"{tup}\n"
+        continue
+    model_name, model_name_canonical = tup
     res = main(Namespace(
         output_file=f"results/{model_name.replace('/', '_')}_results.csv",
         model_name=None,
